d89c5ec1724d846cd2298643f5c52e6c1aae8616
rr69sport <richardramsua@gmail.com>
2020-10-23 15:39:57 -0300

    first commit

--- test.js
+++
@@ -1,8 +0,0 @@
-const maths = require('./index.js')
-
-const a = maths.add(1, 2)
-const b = maths.divide(1, 2)
-const c = maths.multiply(1, 2)
-const d = maths.substract(1, 2)
-
-console.log(a, b, c, d)
