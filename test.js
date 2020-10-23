29f8e0616977d2e8361afc78c265af40e80c6f80
rr69sport <richardramsua@gmail.com>
2020-10-23 11:49:47 -0300

    fix methos calls

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const maths = require('./index.js')
 
-const a = maths.add(1,2)
-const b = maths.add(1,2)
-const c = maths.add(1,2)
-const d = maths.add(1, 2)
+const a = maths.add(1, 2)
+const b = maths.divide(1, 2)
+const c = maths.multiply(1, 2)
+const d = maths.substract(1, 2)
 
 console.log(a, b, c, d)
