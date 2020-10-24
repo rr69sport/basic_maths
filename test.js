be81dcd82718dc0c72104f4dc72011cab5458c51
rr69sport <richardramsua@gmail.com>
2020-10-24 00:08:26 -0300

    the code is rewritten as it had been deleted for some reason

---
+++ test.js
@@ -0,0 +1,8 @@
+const maths = require("./index.js");
+
+const a = maths.add(1, 2);
+const b = maths.divide(1, 2);
+const c = maths.multiply(1, 2);
+const d = maths.substract(1, 2);
+
+console.log(a, b, c, d);
