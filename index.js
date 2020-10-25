155e1998db926d83d8e54809251a136bafe5fede
rr69sport <richardramsua@gmail.com>
2020-10-24 21:20:38 -0300

    the validation of parameters is changed with the "isNaN()" function, by the "typeof" operator

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-
 const validateNumbers = (a, b) => {
-    if (!isNaN(a) && !isNaN(b)) {
+
+    if (typeof a === 'number' && typeof b === 'number') {
+
         return true;
-    } else {
-        return false;
+
     }
+    return false;
+
 };
 
 const errorMessage = (a, b) => {
-    if (isNaN(a)) {
+
+    if (typeof a !== 'number') {
+
         console.error(`"${a}" is not a number`);
+
     }
-    if (isNaN(b)) {
+    if (typeof b !== 'number') {
+
         console.error(`"${b}" is not a number`);
+
     }
     return false;
+
 };
 
 const maths = {
@@ -22,64 +30,78 @@ const maths = {
      * Add
      * @param {number} a number
      * @param {number} b number
-     * @returns {number} a + b
+     * @returns {number} a + b // return false if one or both paramters is not number
      * @example maths.add(1, 2) // return 3
      */
     add(a, b) {
+
         if (validateNumbers(a, b)) {
+
             return a + b;
-        } else {
-            return errorMessage(a, b);
+
         }
+        return errorMessage(a, b);
+
     },
 
     /**
      * Substract
      * @param {number} a number
      * @param {number} b number
-     * @returns {number} a - b
+     * @returns {number} a - b return false if one or both paramters is not number
      * @example maths.substract(1,2) // return -1
      */
     substract(a, b) {
+
         if (validateNumbers(a, b)) {
+
             return a - b;
-        } else {
-            return errorMessage(a, b);
+
         }
+        return errorMessage(a, b);
+
     },
 
     /**
      * Multiply
      * @param {number} a number
      * @param {number} b number
-     * @returns {number} a * b
+     * @returns {number} a * b return false if one or both paramters is not number
      * @example maths.multiply(1,2) // return 2
      */
     multiply(a, b) {
+
         if (validateNumbers(a, b)) {
+
             return a * b;
-        } else {
-            return errorMessage(a, b);
+
         }
+        return errorMessage(a, b);
+
     },
 
     /**
      * Divide
      * @param {number} a number
      * @param {number} b number
-     * @returns {number} a / b
+     * @returns {number} a / b return false if one or both paramters is not number
      * @example maths.divide(1,2) // return 0.5
      */
     divide(a, b) {
+
         if (b === 0) {
+
             console.error(`Division by "${b}" is not possible.`);
             return false;
+
         }
         if (validateNumbers(a, b)) {
+
             return a / b;
-        } else {
-            return errorMessage(a, b);
+
         }
+        return errorMessage(a, b);
+
     },
 };
 module.exports = maths;
