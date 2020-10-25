54a00623573edca1a39f6b177d3d11ec463b0fdb
rr69sport <richardramsua@gmail.com>
2020-10-24 21:26:22 -0300

    update version to 1.0.10

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-const maths = require("./index.js");
+const maths = require('./index.js');
 
-const a = maths.add(1, 2);
+const a = maths.add('1', 2);
 const b = maths.divide(1, 2);
-const c = maths.multiply(1, 2);
+const c = maths.multiply(1, '2');
 const d = maths.substract(1, 2);
 
+// eslint-disable-next-line no-console
 console.log(a, b, c, d);
